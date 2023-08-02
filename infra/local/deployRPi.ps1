66af4ec2c860312f508f32d6860c4eb78ef13ab9
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2023-08-02 11:59:41 -0400

    Reduce manual steps needed to deploy to RPi.

--- infra/local/deployRPi.ps1
+++ infra/local/deployRPi.ps1
@@ -1,10 +1,19 @@
+$publishDirectory = ".\src\MrCapitalQ.Sleepi.Api\bin\Release\net7.0\linux-arm64\publish"
+
+# Clean publish directory
+Remove-Item -LiteralPath $publishDirectory -Force -Recurse -ErrorAction Ignore
+
 # Build and publish app for Raspberry Pi OS 64 bit.
 dotnet publish .\src\MrCapitalQ.Sleepi.Api --configuration Release --runtime linux-arm64  --self-contained true
 
-# Copy app to Raspberry Pi deploy destination
-scp -r .\src\MrCapitalQ.Sleepi.Api\bin\Release\net7.0\linux-arm64\publish\* q@raspberrypi:/var/www/Sleepi
+# Include service definition file in publish directory.
+Copy-Item .\infra\linux\kestrel-sleepi.service $publishDirectory
 
-# Copy service definition file to Raspberry Pi deploy destination
-scp -r .\infra\linux\kestrel-sleepi.service q@raspberrypi:/var/www/Sleepi
+# Include service definition file in publish directory.
+Copy-Item .\infra\linux\configure.sh $publishDirectory
 
-# SSH into Raspberry Pi and run commands in infra/linux/configure.sh
+# Copy publish directory to Raspberry Pi deploy destination
+scp -r $publishDirectory\* q@raspberrypi:/var/www/Sleepi
+
+# SSH into Raspberry Pi and execute the following command
+# sudo bash /var/www/Sleepi/configure.sh
